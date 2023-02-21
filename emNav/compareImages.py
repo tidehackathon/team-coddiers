a27851cf17a9c2d871cad15d6e21eb362cd4efdc
Patryk Barczak <patryk.barczak@wat.edu.pl>
2023-02-21 18:34:59 +0100

    Refactor code and add param files

--- emNav/compareImages.py
+++ emNav/compareImages.py
@@ -1,14 +1,11 @@
 import math
-
 import cv2 as cv
 import concurrent.futures
-
 import numpy as np
+from params import flann_index_kdtree, search_params_checks, flann_index_trees, dist_calc_accuracy
 
-accuracy = 100
-flann_index_kdtree = 1
-index_params = dict(algorithm=flann_index_kdtree, trees=5)
-search_params = dict(checks=50)
+index_params = dict(algorithm=flann_index_kdtree, trees=flann_index_trees)
+search_params = dict(checks=search_params_checks)
 sift = cv.SIFT_create()
 flann = cv.FlannBasedMatcher(index_params, search_params)
 
@@ -46,12 +43,12 @@ def compare_images(first_img, second_img, mono, center_point):
     compared_images = cv.drawMatches(first_img, key_points_1, second_img, key_points_2, good_matches[:1], img_matches,
                                      flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-    best_key_points_1 = [key_points_1[m.queryIdx] for m in good_matches][:accuracy]
-    best_key_points_2 = [key_points_2[m.trainIdx] for m in good_matches][:accuracy]
+    best_key_points_1 = [key_points_1[m.queryIdx] for m in good_matches][:dist_calc_accuracy]
+    best_key_points_2 = [key_points_2[m.trainIdx] for m in good_matches][:dist_calc_accuracy]
 
     dist_difference = 0
-    if len(best_key_points_1) > accuracy and len(best_key_points_2) > accuracy:
-        for i in range(accuracy):
+    if len(best_key_points_1) > dist_calc_accuracy and len(best_key_points_2) > dist_calc_accuracy:
+        for i in range(dist_calc_accuracy):
             dist_one = math.dist(center_point, best_key_points_1[i].pt)
             dist_two = math.dist(center_point, best_key_points_2[i].pt)
             dist_difference += math.fabs(dist_one - dist_two)
@@ -67,7 +64,7 @@ def compare_images(first_img, second_img, mono, center_point):
                 dist_two = math.dist(center_point, best_key_points_2[i].pt)
                 dist_difference += dist_one - dist_two
 
-    dist_difference = dist_difference / (accuracy * 100)
+    dist_difference = dist_difference / (dist_calc_accuracy * 100)
 
     print(dist_difference)
 
--- emNav/main.py
+++ emNav/main.py
@@ -1,61 +1,6 @@
-import cv2
+from emNav.videoReader import video_reader
 
-from compareImages import compare_images
-import matplotlib.pyplot as plt
+drone_height = 0
+compas_angle = 0
 
-from emNav.rescaleFrame import rescale_frame
-
-vic_cap = cv2.VideoCapture('rec\sample2a-gimbal.MOV')
-last_image = None
-compared_images = None
-first_step = True
-count = 0
-current_x = 0
-current_y = 0
-current_z = 0
-x_points = []
-y_points = []
-z_points = []
-
-plt.ion()
-fg = plt.figure()
-ax = fg.gca()
-h = None
-fg.show()
-
-fg2 = plt.figure()
-ax2 = fg2.add_subplot(projection='3d')
-ax2.set_xlim(-10, 10)
-ax2.set_ylim(-10, 10)
-ax2.set_zlim(0, 10)
-ax2.set_xlabel('$X$')
-ax2.set_ylabel('$Y$')
-ax2.set_zlabel('$Z$')
-h2 = ax2.scatter(x_points, y_points, z_points)
-fg2.show()
-
-success, image = vic_cap.read()
-center_width = int(image.shape[1] / 2)
-center_height = int(image.shape[0] / 2)
-center_point = (center_width, center_height)
-while success:
-    success, image = vic_cap.read()
-    image = rescale_frame(image, percent=50)
-    if last_image is not None:
-        compared_images, dist_difference = compare_images(last_image, image, True, center_point)
-        current_x = current_x + dist_difference
-        x_points.append(current_x)
-        y_points.append(current_y)
-        z_points.append(current_z)
-        h2._offsets3d = (x_points, y_points, z_points)
-        if first_step:
-            h = ax.imshow(compared_images)
-            first_step = False
-        else:
-            h.set_data(compared_images)
-        plt.draw(), plt.pause(0.05)
-    last_image = image
-    count += 1
-
-vic_cap.release()
-cv2.destroyAllWindows()
+video_reader(drone_height, compas_angle)
