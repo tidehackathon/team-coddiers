1e1f738a357d96fcfe3ab31e027bb4ad8f85adc4
Patryk Barczak <patryk.barczak@wat.edu.pl>
2023-02-21 16:17:59 +0100

    Add script to draw pre-calculated points

--- pythonFakeGps/compareImages.py
+++ pythonFakeGps/compareImages.py
@@ -1,5 +1,16 @@
 import cv2 as cv
-import numpy as np
+import concurrent.futures
+
+
+accuracy = 100
+flann_index_kdtree = 1
+index_params = dict(algorithm=flann_index_kdtree, trees=5)
+search_params = dict(checks=50)
+sift = cv.SIFT_create()
+
+
+def generate_data_img(img):
+    return sift.detectAndCompute(img, None)
 
 
 def get_distance(item):
@@ -11,14 +22,13 @@ def compare_images(first_img, second_img, mono):
         first_img = cv.cvtColor(first_img, cv.COLOR_BGR2GRAY)
         second_img = cv.cvtColor(second_img, cv.COLOR_BGR2GRAY)
 
-    sift = cv.SIFT_create()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        t1 = executor.submit(generate_data_img, first_img)
+        t2 = executor.submit(generate_data_img, second_img)
 
-    key_points_1, descriptors_1 = sift.detectAndCompute(first_img, None)
-    key_points_2, descriptors_2 = sift.detectAndCompute(second_img, None)
+        key_points_1, descriptors_1 = t1.result()
+        key_points_2, descriptors_2 = t2.result()
 
-    flann_index_kdtree = 1
-    index_params = dict(algorithm=flann_index_kdtree, trees=5)
-    search_params = dict(checks=100)
     flann = cv.FlannBasedMatcher(index_params, search_params)
     matches = flann.knnMatch(descriptors_1, descriptors_2, k=2)
 
@@ -26,10 +36,18 @@ def compare_images(first_img, second_img, mono):
     for m, n in matches:
         if m.distance < 0.7 * n.distance:
             good_matches.append(m)
+    good_matches = sorted(good_matches, key=lambda x: x.distance)
 
-    img_matches = np.empty((max(first_img.shape[0], second_img.shape[0]), first_img.shape[1] + second_img.shape[1], 3),
-                           dtype=np.uint8)
-    compared_images = cv.drawMatches(first_img, key_points_1, second_img, key_points_2, good_matches, img_matches,
-                                     flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    best_key_points_1 = [key_points_1[m.queryIdx] for m in good_matches][:accuracy]
+    best_key_points_2 = [key_points_2[m.trainIdx] for m in good_matches][:accuracy]
 
-    return compared_images
+    x_difference = 0
+    y_difference = 0
+    for i in range(accuracy):
+        x_difference += best_key_points_2[i].pt[0] - best_key_points_1[i].pt[0]
+        y_difference += best_key_points_2[i].pt[1] - best_key_points_1[i].pt[1]
+
+    x_difference = x_difference / (accuracy * 100)
+    y_difference = y_difference / (accuracy * 100)
+
+    return x_difference, y_difference
--- pythonFakeGps/main.py
+++ pythonFakeGps/main.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np
+import time
 
 from compareImages import compare_images
 import matplotlib.pyplot as plt
@@ -9,40 +9,38 @@ last_image = None
 compared_images = None
 first_step = True
 count = 0
+current_x = 0
+current_y = 0
+current_z = 0
 x_points = []
 y_points = []
 z_points = []
 
 plt.ion()
-fg = plt.figure()
-ax = fg.gca()
-h = None
-fg.show()
-
 fg2 = plt.figure()
 ax2 = fg2.add_subplot(projection='3d')
-ax2.set_xlim(0, 100)
-ax2.set_ylim(0, 100)
-ax2.set_zlim(0, 100)
+ax2.set_xlim(-10, 10)
+ax2.set_ylim(-10, 10)
+ax2.set_zlim(0, 10)
+ax2.set_xlabel('$X$')
+ax2.set_ylabel('$Y$')
+ax2.set_zlabel('$Z$')
 h2 = ax2.scatter(x_points, y_points, z_points)
 fg2.show()
 
 success, image = vic_cap.read()
 while success:
+    start = time.time()
     success, image = vic_cap.read()
-    print('[', count, '] Read a new frame: ', success)
     if last_image is not None:
-        compared_images = compare_images(last_image, image, True)
-        x_points.append(count)
-        y_points.append(count)
-        z_points.append(count)
+        x_difference, y_difference = compare_images(last_image, image, True)
+        current_x = current_x + x_difference
+        current_y = current_y + y_difference
+        x_points.append(current_x)
+        y_points.append(current_y)
+        z_points.append(current_z)
         h2._offsets3d = (x_points, y_points, z_points)
-        if first_step:
-            h = ax.imshow(compared_images)
-            first_step = False
-        else:
-            h.set_data(compared_images)
-            # plt.draw(), plt.pause(1e-3)
-            plt.draw(), plt.pause(1)
+        plt.draw(), plt.pause(0.05)
     last_image = image
     count += 1
+    print(time.time() - start)
